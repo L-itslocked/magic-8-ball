5548ca497c32530f282bbb205551ed6a37633feb
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-07-27 21:53:52 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let magicEight = '1'
+switch(magicEight) {
+    case 1: 
+    return;
+    break;
+    case 2: 
+    return;
+    break;
+    case 3: 
+    return;
+    break;
+    case 4: 
+    return;
+    break;
+    case 5: 
+    return;
+    break;
+    case 6: 
+    return;
+    break;
+    case 7: 
+    return;
+    break;
+    case 8: 
+    return;
+    break;
+    
+}
