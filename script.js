a3dc311839a53ccc4e95fc70210bc2a663af71c0
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-07-27 22:31:51 -0400

    Add switch

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-let magicEight = '1'
-switch(magicEight) {
-    case 1: 
-    return;
-    break;
-    case 2: 
-    return;
-    break;
-    case 3: 
-    return;
-    break;
-    case 4: 
-    return;
-    break;
-    case 5: 
-    return;
-    break;
-    case 6: 
-    return;
-    break;
-    case 7: 
-    return;
-    break;
-    case 8: 
-    return;
-    break;
-    
+function shakeMagicEightBall() {
+  const ball = document.querySelector(".magic-ball");
+  let magicEight = Math.ceil(Math.random() * 9);
+
+  switch (magicEight) {
+    case 0:
+      ball.innerHTML = "girl, byeeeee";
+      break;
+    case 1:
+      ball.innerHTML = "Yes";
+      break;
+    case 2:
+      ball.innerHTML = "No";
+      break;
+    case 3:
+      ball.innerHTML = "Maybe";
+      break;
+    case 4:
+      ball.innerHTML = "Very Doubtful";
+      break;
+    case 5:
+      ball.innerHTML = "Ask again later";
+      break;
+    case 6:
+      ball.innerHTML = "Reply hazy, try again";
+      break;
+    case 7:
+      ball.innerHTML = "Most likely";
+      break;
+    case 8:
+      ball.innerHTML = "My sources say no";
+
+      break;
+  }
 }
+
+shakeMagicEightBall();
